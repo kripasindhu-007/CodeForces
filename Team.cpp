9eff86164f9a7a0fa0eb53c118e8d21e1075fa8c
Kripa Sindhu <sindhukripa007@gmail.com>
2022-03-09 18:49:40 +0530

    verified

--- Team.cpp
+++ Team.cpp
@@ -3,7 +3,19 @@ using namespace std;
 int main()
 {
     int prlb;
-    cin>>prlb;//number of problem
     
+    int counter=0;
+    cin>>prlb;//number of problem
+    while(prlb--){
+        int a,b,c;//storing sureity of three friends
+        cin>>a>>b>>c;
+        if((a==1&&b==1&&c==0)||(b==1&&c==1&&a==0)||(c==1&&a==1&&b==0))
+        counter++;
+        if(a==1&&b==1&&c==1)
+        counter++;
+        
+    }
+   
+    cout<<counter;
 return 0;
 }
